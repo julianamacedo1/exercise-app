d03e20986004b4c999e3179aa2500f1d3ba9d552
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 14:31:59 -0500

    March 6, 2021 2:31 PM

--- src/components/TotalExercise/index.js
+++ src/components/TotalExercise/index.js
@@ -3,6 +3,7 @@ import React from "react"
 export default class TotalExercise extends React.Component {
     constructor(props) {
         super(props)
+        this.state = { value: this.props.value }
     }
     addOne() {
         this.setState((prevState) => {
