aaa40be9ecafa0518dc046396cb475fd69843539
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 14:36:56 -0500

    March 6, 2021 2:36 PM

--- src/components/TotalExercise/index.js
+++ src/components/TotalExercise/index.js
@@ -3,6 +3,12 @@ import React from "react"
 export default class TotalExercise extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {minutes: ""}
+        this.state = { minutes: '' }
     }
+
+    handleChange = event => {
+        this.setState({ username: event.target.value })
+    }
+
+    render
 }
