a3ac43dbafd7faf0891becbe067609023468de10
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 15:00:45 -0500

    March 6, 2021 3:00 PM

--- src/components/TotalExercise/index.js
+++ src/components/TotalExercise/index.js
@@ -18,12 +18,11 @@ export default class TotalExercise extends React.Component {
                     style={{ "margin": "5px", "font-family": "courier"}}
                     type="number" 
                     value={this.state.minutes}
-                    onChange={this.handleChange}
                 >
                 </input>
                 <button style={{ "margin": "3px", "font-family": "courier" }}>Submit</button>
             </form>
-                <p onClick() => () style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>total time: {this.state.minutes}</p>   
+                <p onClick={() => this.handleChange} style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>total time: {this.state.minutes}</p>   
             </>
         )
     }
