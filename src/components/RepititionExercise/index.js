04f8d6f3b62a3f8c6171b11aecdfae6ac1d79f29
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 13:49:05 -0500

    March 6, 2021 1:49 PM

---
+++ src/components/RepititionExercise/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+export default class RepetitionExercise extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { value: this.props.value }
+    }
+    addOne() {
+        this.setState((prevState) => {
+            let newValue = prevState.value + 1
+            this.props.updateValue(newValue)
+            return {
+                value: newValue,
+            }
+        })
+    }
+    reset() {
+        this.setState((newValue) => {
+            let resetValue = newValue.value = 0
+            this.props.updateValue(resetValue)
+            return {
+                value: resetValue,
+            }
+        })
+    }
+    render() {
+        return (
+            <>
+                <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>{this.props.name}</p>
+                <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>Reps: {this.state.value}</p>
+                <button onClick={() => this.addOne()} style={{ "margin": "3px", "font-family": "courier" }}>Complete Rep</button>
+                <button onClick={() => this.reset()} style={{ "margin": "3px", "font-family": "courier" }}>Reset</button>
+            </>
+        )
+    }
+}
