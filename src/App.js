487827c4eef12a8ea10f2cf0b2c1a8db12aa41c1
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 13:52:57 -0500

    March 6, 2021 1:52 PM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,66 @@
+//https://medium.com/@peterjd42/building-timers-in-react-stopwatch-and-countdown-bc06486560a2
+
 import React from "react"
+
+export default class Stopwatch extends React.Component {
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0
+    };
+
+    startTimer = () => {
+        this.setState({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+        this.timer = setInterval(() => {
+            this.setState({
+                timerTime: Date.now() - this.state.timerStart
+            });
+        }, 10);
+    };
+
+    stopTimer = () => {
+        this.setState({ timerOn: false });
+        clearInterval(this.timer);
+    };
+
+    resetTimer = () => {
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        });
+    };
+
+    render() {
+        const { timerTime } = this.state;
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+        return (
+            <>
+                <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>
+                    Timer: {minutes} : {seconds} : {centiseconds}
+                </p>
+                {this.state.timerOn === false && this.state.timerTime === 0 && (
+                    <button onClick={this.startTimer} style={{ "margin": "3px", "font-family": "courier" }}>Start</button>
+                )}
+                {this.state.timerOn === true && (
+                    <button onClick={this.stopTimer} style={{ "margin": "3px", "font-family": "courier" }}>Stop</button>
+                )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                    <button onClick={this.startTimer} style={{ "margin": "3px", "font-family": "courier" }}>Resume</button>
+                )}
+                {this.state.timerOn === false && this.state.timerTime > 0 && (
+                    <button onClick={this.resetTimer} style={{ "margin": "3px", "font-family": "courier" }}>Reset</button>
+                )}
+            </>   
+        );
+    }
+}
+
 import RepetitionExercise from "./components/RepetitionExercise"
 import DurationExercise from "./components/DurationExercise"
 
