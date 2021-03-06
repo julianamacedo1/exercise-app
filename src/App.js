9dac355c953692ec7dd0ee4cad88f23518157cb5
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 14:56:27 -0500

    March 6, 2021 2:56 PM

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class MenuScreen extends React.Component {
                 </li>
               )}
             </ul>
-            <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>Minutes spent exercising today:</p>
+            <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>Total minutes spent exercising today:</p>
             <TotalExercise></TotalExercise>
           </>
         )
