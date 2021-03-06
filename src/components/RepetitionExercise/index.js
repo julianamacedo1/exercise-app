ed378fe08b811c9ef96fb395c5df37bf8c6452de
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 16:02:49 -0500

    March 6, 2021 4:02 PM

--- src/components/RepetitionExercise/index.js
+++ src/components/RepetitionExercise/index.js
@@ -28,8 +28,8 @@ export default class RepetitionExercise extends React.Component {
             <>
                 <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>{this.props.name}</p>
                 <p style={{ "margin": "10px 5px 5px 5px", "font-family": "courier" }}>Reps: {this.state.value}</p>
-                <button onClick={() => this.addOne()} style={{ "margin": "3px", "font-family": "courier" }}>Complete Rep</button>
-                <button onClick={() => this.reset()} style={{ "margin": "3px", "font-family": "courier" }}>Reset</button>
+                <button onClick={() => this.addOne()} >Complete Rep</button>
+                <button onClick={() => this.reset()} >Reset</button>
             </>
         )
     }
