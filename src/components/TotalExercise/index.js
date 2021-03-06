d78eed9351340d07cca0835dc82699344342f438
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-06 15:19:05 -0500

    March 6, 2021 3:19 PM

--- src/components/TotalExercise/index.js
+++ src/components/TotalExercise/index.js
@@ -9,6 +9,7 @@ export default class TotalExercise extends React.Component {
     addMinutes() {
         this.setState((oldTime) => {
             let newTime = oldTime.value
+            this.props.updateTime
         }
 
         )
